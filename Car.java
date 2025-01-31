eea54b41f8378bbd5bed1101361515a5c26872e7
AbhiKr82 <abhijais8287@gmail.com>
2025-01-31 22:20:23 +0530

    Initil Commit

---
+++ Car.java
@@ -0,0 +1,13 @@
+package LocalRepo;
+
+class Car {
+    String Brand;
+    String Model;
+    int price;
+
+    Car(String b,String m,int p){
+        this.Brand=b;
+        this.Model=m;
+        this.price=p;
+    }
+}
